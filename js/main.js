1a1f7ab8c39db2e0b218ad8b0dc4bbd0cfdb44b8
ElmarNi <46805869+ElmarNi@users.noreply.github.com>
2020-10-05 13:21:44 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+  $('#slider .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    items: 1,
+    dots: true,
+    nav: true
+  });
+  $('#projects .owl-carousel').owlCarousel({
+    center: true,
+    loop: true,
+    margin: 20,
+    items: 1.3,
+    dots: true,
+    nav: true
+  });
+  $('#referanceLogos .owl-carousel').owlCarousel({
+    loop: true,
+    items: 1,
+    dots: false,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000
+  });
+  $('#productcategories .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    items: 5,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout: 3000
+  });
+  $("header .open-menu").click(function (e) {
+    e.preventDefault();
+    $("header .open-menu i").addClass("d-none");
+    $(".small-screen-menu-nav nav").addClass("active");
+    $("body").addClass("overflow-hidden");
+    $(".small-screen-overlay").addClass("active");
+  });
+  $(".small-screen-menu-nav .close-menu").click(function (e) {
+    e.preventDefault();
+    $("header .open-menu i").removeClass("d-none");
+    $(".small-screen-menu-nav nav").removeClass("active");
+    $("body").removeClass("overflow-hidden");
+    $(".small-screen-overlay").removeClass("active");
+    $(".small-screen-menu-nav .submenu ul").removeClass("active");
+    $(".small-screen-menu-nav .submenu i").removeClass("active");
+  });
+  $(".small-screen-menu-nav .submenu a").click(function (e) {
+    e.preventDefault();
+    $(".small-screen-menu-nav .submenu ul").toggleClass("active");
+    $(this).parent().toggleClass("active");
+    $(this).children().last().toggleClass("active");
+  });
+  $("#large-screens .languages").click(function (e) {
+    e.preventDefault();
+    $("#large-screens .languages ul").toggleClass("active");
+    $("#large-screens .languages i").toggleClass("active");
+  });
+  $("#large-screens .languages").first().click(function (e) {
+    e.preventDefault();
+  });
+  getMobileOperatingSystem()
+});
+$(window).resize(function () {
+  if ($(".small-screen-menu-nav nav").hasClass("active")) {
+    $("header .open-menu i").removeClass("d-none");
+    $(".small-screen-menu-nav nav").removeClass("active");
+    $("body").removeClass("overflow-hidden");
+    $("header .open-menu").removeClass("d-none");
+    $(".small-screen-overlay").removeClass("active");
+  }
+
+  if (!$(".small-screen-menu-nav nav").hasClass("active")) {
+    $(".small-screen-menu-nav .submenu ul").removeClass("active");
+    $(".small-screen-menu-nav .submenu i").removeClass("active");
+  }
+});
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        $("#callToaction").addClass("bgAtch")
+        $("#about .right-side").addClass("bgAtch")
+    }
+}
